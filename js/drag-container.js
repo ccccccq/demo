948b16472f6105d7ccdb6d9db1d07d778cecdd27
陈淇 <chenqi5@asiainfo.com>
2020-04-18 20:07:12 +0800

    20200418-毕设-拖拽界面

--- js/drag-container.js
+++ js/drag-container.js
@@ -21,58 +21,11 @@ var desTop = $(".gain").offset().top;
 var desLeft = $(".gain").offset().left;
 var desWidth =  $(".gain").width();
 var desHeight  =  $(".gain").height();
+//是否是从外到内移动的
+var regionMove =true;
 //鼠标右键重写
 // $(document).ready(function(){
-//     var rcm = window.RMenu;
-//     rcm.init({
-//         area:'body',
-//         items:{
-//             "copy":{name:"复制",icon:'copy'},
-//             "del":{name:"删除",icon:'trash-o'},
-//             "paste":{name:"粘贴",icon:'paste'},
-//             "undo":{name:"撤回",icon:'undo'}
-//         },
-//         callback:function(res){
-//             if(res.data == 'paste'){
-//                 if(newEle && newEle.hasClass("disNone"))
-//                 newEle.css("display","");
-//                 //变成原来的样式
-//                 newEle.appendTo(".gain");
-//                 $(document).unbind('mousedown').unbind('mouseup'); }
-//             else if(res.data == 'undo'){
-//                 if(undoEle)
-//                 //撤回
-//                 undoEle.css("display",""); }
-//             $('.draggable').each(function(){
-//                 objX = $(this).offset().left  ;
-//                 objY = $(this).offset().top ;
-//                 var width = $(this).width();
-//                 var height = $(this).height();
-//                 $(document).mousedown(function(event){ //获取鼠标按下的位置
-//                     if(event.which == 3 ){
-//                         rightX = event.pageX;
-//                         rightY = event.pageY;
-//                     }
-//                 });
-//                 if( rightX<objX +width && rightX >objX && rightY<objY+height && rightY>objY ){
-//                     if(res.data == 'del') {
-//                        undoEle = $(this);
-//                        $(this).addClass("disNone"); //暂时隐藏
-//                     }}
-//                 else  if(res.data == 'copy'){
-//                     newEle =  cloneEle.clone(true);
-//                     $(document).mousedown(function(event){ //获取鼠标按下的位置
-//                         x = event.pageX;
-//                         y = event.pageY;
-//                     });
-//                     $(document).mouseup(function(event){//鼠标释放
-//                         newEle.css("left",x );
-//                         newEle.css("top",y );
-//                     })
-//                 }
-//             })
-//         }
-//     })
+//
 // })
 //左边种类展示的变成立即执行函数字
 (function show(){
@@ -101,6 +54,56 @@ var desHeight  =  $(".gain").height();
     }
 }());
 $(window).load(function(evt) {
+    var rcm = window.RMenu;
+    rcm.init({
+        area:'body',
+        items:{
+            "copy":{name:"复制",icon:'copy'},
+            "del":{name:"删除",icon:'trash-o'},
+            "paste":{name:"粘贴",icon:'paste'},
+            "undo":{name:"撤回",icon:'undo'}
+        },
+        callback:function(res){
+            if(res.data == 'paste'){
+                if(newEle && newEle.hasClass("disNone"))
+                    newEle.css("display","");
+                //变成原来的样式
+                newEle.appendTo(".gain");
+                $(document).unbind('mousedown').unbind('mouseup'); }
+            else if(res.data == 'undo'){
+                if(undoEle)
+                //撤回
+                    undoEle.css("display",""); }
+            $('.draggable').each(function(){
+                objX = $(this).offset().left  ;
+                objY = $(this).offset().top ;
+                var width = $(this).width();
+                var height = $(this).height();
+                $(document).mousedown(function(event){ //获取鼠标按下的位置
+                    if(event.which == 3 ){
+                        rightX = event.pageX;
+                        rightY = event.pageY;
+                    }
+                });
+                if( rightX<objX +width && rightX >objX && rightY<objY+height && rightY>objY ){
+                    if(res.data == 'del') {
+                        undoEle = $(this);
+                        $(this).addClass("disNone"); //暂时隐藏
+                    }}
+                else  if(res.data == 'copy'){
+                    newEle =  cloneEle.clone(true);
+                    $(document).mousedown(function(event){ //获取鼠标按下的位置
+                        x = event.pageX;
+                        y = event.pageY;
+                    });
+                    $(document).mouseup(function(event){//鼠标释放
+                        newEle.css("left",x );
+                        newEle.css("top",y );
+                    })
+                }
+            })
+        }
+    })
     //点击后显示相应区域内容
     $(".draggable").attr("isDrag",false);
         $(".draggable").each(function(index,elem){
@@ -109,19 +112,22 @@ $(window).load(function(evt) {
                 //鼠标初始化位置
                 initMouseX = evt.pageX;
                 initMouseY = evt.pageY;
+                regionMove = false;
                 if(!$(this).hasClass('cloneEle')) {
                     cloneEle = $(this).clone(true);
                     cloneEle.addClass("cloneEle");
                     $(this).before(cloneEle);
+                    regionMove = true;
                 }
                 //元素的初始化位置
                 initObjX = $(this).offset().left;
                 initObjY = $(this).offset().top;
                 $(this).attr("name","" + userId + getDate());
                 //鼠标按下标识
-                $(this).attr("isDrag",true)
+                cloneEle.attr("isDrag",true)
             }).on("mousemove",function(evt){
-                if (cloneEle && $(this).attr("isDrag") == "true") {
+                // evt.preventDefault();
+                if (cloneEle && cloneEle.attr("isDrag") == "true") {
                     // 改变位置
                     moveX = evt.pageX - initMouseX + initObjX; //移动的X的位置
                     moveY = evt.pageY - initMouseY + initObjY;   //移动的Y的位置
@@ -137,15 +143,24 @@ $(window).load(function(evt) {
                     moveX = desLeft + desWidth - $(this).width();
                 if(moveY > desTop + desHeight - $(this).height())
                     moveY = desTop + desHeight - $(this).height();
+
                 $(this).css("left",moveX);
                 $(this).css("top",moveY); //移到外面就默认到格子中那行那列
-                $(this).appendTo(".gain");
                 //调用函数addEdit 入参：当前组件品种 返回：相应编辑区的代码
-                addEdit($(this));
+                if(regionMove){
+                    $(this).appendTo(".gain");
+                    addEdit($(this))
+                }
+
             })
         })
     });
 
+$(".compo ul li").click(function(){
+    var indexLi = $(this).index();
+    $(".showBox").eq(indexLi).css("display","");
+    $(".showBox").not($(".showBox").eq(indexLi)).css("display","none");
+})
 
 //得到当前的时间
 function getDate(){
@@ -164,20 +179,22 @@ function getCode(){
 function addEdit(kind){
    var type  = kind.attr("class").split(" ")[0];
    //传给后台 type      得到代码str
-   var tempUrl =  "http://localhost:1337/Edit-Configs?edit_name="+ type;
+   var tempUrl =  "http://localhost:1337/Edit-Configs?edit_compo="+ type;
     var editcode = $.ajax({url:tempUrl,async:false});
     var resp = eval(editcode.responseText);
-   var str = "<div class='edit-container'>字体内容：<input type = 'text' class='edit-input-fontvalue' value='文本'> </div>"
-  $(".edit").html(str);
+    $(".edit-container").html(resp[0].edit_code);
     $(".edit-input-fontvalue").change(function(){
         var value = $(".edit-input-fontvalue").val();
-        kind.val(value);
+        kind.val(value) ;
     })
-
+    $(".edit-text-value").change(function(){
+        var value = $(".dit-text-value").val();
+        kind.text(value) ;
+    })
+    $(".edit-text-size").change(function(){
+        var value = $(".edit-text-size").val();
+        kind.css("font-size",value + "px");
+    })
 }
 
-$(".compo ul li").click(function(){
-    var indexLi = $(this).index();
-    $(".showBox").eq(indexLi).css("display","");
-    $(".showBox").not($(".showBox").eq(indexLi)).css("display","none");
-})
+
